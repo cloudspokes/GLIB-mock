c71d5328988bf856b74355192d89fac41df44769
CWD <cwd@cloudspokes.com>
2016-02-08 22:59:11 -0500

    hard coding this for now till endpoint is working

--- routes/index.js
+++ routes/index.js
@@ -14,18 +14,25 @@ router.post('/', function(req, res, next) {
     console.log('posting to: ', config.APIURL_BASE + config.APIURL_CHALLENGE);
     console.log(req.body);
 
-    request.post(config.APIURL_BASE + config.APIURL_CHALLENGE, {
-        form: req.body,
-        function(err, httpResponse, body) {
-            if (err) {
-                console.log(err);
-                res.state(500).json(body);
-            } else {
-                console.log(body);
-                res.json(body);
-            }
-        }
+    res.json({
+        success: true,
+        challengeURL: 'http://topcoder.com',
+        createdDate: new Date()
     });
+    /*
+        request.post(config.APIURL_BASE + config.APIURL_CHALLENGE, {
+            form: req.body,
+            function(err, httpResponse, body) {
+                if (err) {
+                    console.log(err);
+                    res.state(500).json(body);
+                } else {
+                    console.log(body);
+                    res.json(body);
+                }
+            }
+        });
+    */
 });
 
 
